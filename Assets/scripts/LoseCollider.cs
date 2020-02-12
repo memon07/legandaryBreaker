59b68c38fa2641da4b436e0fd404eab52746a0b0
Memon Shoyeb <memonshoyeb26@gmail.com>
2020-02-12 22:18:25 +0530

    added level

--- Assets/scripts/LoseCollider.cs
+++ Assets/scripts/LoseCollider.cs
@@ -7,6 +7,6 @@ public class LoseCollider : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene("endingScene");
+        SceneManager.LoadScene("startingScene");
     }
 }
