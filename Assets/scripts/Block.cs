b06ab8471c4d80c3beddb132e5e7706bff2651ac
Memon Shoyeb <memonshoyeb26@gmail.com>
2020-02-13 00:24:20 +0530

    added sound effects

--- Assets/scripts/Block.cs
+++ Assets/scripts/Block.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
+    //cached reference
+    level lev;
+
+    private void Start()
+    {
+        lev = FindObjectOfType<level>();
+        lev.CountBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
