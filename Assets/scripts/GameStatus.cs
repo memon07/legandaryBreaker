f0a29220f90d5ce38b5af1ea7d2f44ae6db5480e
Memon Shoyeb <memonshoyeb26@gmail.com>
2020-02-14 00:25:02 +0530

    score added + scene carried to

--- Assets/scripts/GameStatus.cs
+++ Assets/scripts/GameStatus.cs
@@ -11,6 +11,21 @@ public class GameStatus : MonoBehaviour
     [SerializeField] int currentScore = 0;
     [SerializeField] TextMeshProUGUI scoreText;
 
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if(gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     private void Start()
     {
         scoreText.text = currentScore.ToString();
