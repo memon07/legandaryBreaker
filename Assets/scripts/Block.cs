b2d5c3ef2a19026229153dd0a4fc4fa013423376
Memon Shoyeb <memonshoyeb26@gmail.com>
2020-02-19 00:01:52 +0530

    breakable with unbreakable blocks

--- Assets/scripts/Block.cs
+++ Assets/scripts/Block.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] Sprite[] hitSprites;
+    
+
+    [SerializeField] int timesHit;
 
     //cached reference
     level lev;
@@ -13,12 +18,41 @@ public class Block : MonoBehaviour
     private void Start()
     {
         lev = FindObjectOfType<level>();
-        lev.CountBreakableBlocks();
+        if (tag == "Breakable")
+        {
+            lev.CountBreakableBlocks();
+        }
         gameStatus = FindObjectOfType<GameStatus>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if(tag == "Breakable")
+        {
+            int maxHits = hitSprites.Length + 1;
+            timesHit++;
+            if(timesHit >= maxHits)
+            {
+                DestroyBlock();
+            }
+            else
+            {
+                ShowNextHitSprite();
+            }
+        }
+       
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if(hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("block sprite missing!");
+        }
     }
 
     private void DestroyBlock()
